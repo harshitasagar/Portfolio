82c84d99b38634010bb91f7ae29ce75787d81494
Harshita Sagar <harshitasagar555@gmail.com>
2023-07-27 22:06:26 +0530

    first commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import CanvasLoader from "../Loader";
 
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF('public/desktop_pc/scene.gltf')
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   
 
   return (
